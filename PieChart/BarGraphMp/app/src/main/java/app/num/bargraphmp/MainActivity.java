ea70ed917ba5029057abfcd1aec90836d37e92cf
jessica486424 <jessica486424@gmail.com>
2016-11-20 16:22:19 +0800

    change2

---
+++ PieChart/BarGraphMp/app/src/main/java/app/num/bargraphmp/MainActivity.java
@@ -0,0 +1,87 @@
+package app.num.bargraphmp;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private Random random;//用於產生隨機數
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BarChart barChart = (BarChart) findViewById(R.id.chart);
+       // HorizontalBarChart barChart= (HorizontalBarChart) findViewById(R.id.chart);
+        random = new Random();//隨機數
+        ArrayList<BarEntry> entries = new ArrayList<>();  //輸入總金額數值
+        for (int i = 0; i < 12; i++) {
+            float profit = random.nextFloat() * 1000;
+            //entries.add(BarEntry(float val,int positon);
+            entries.add(new BarEntry(profit, i));
+        }
+
+        BarDataSet dataset = new BarDataSet(entries, "每月花費金額");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+        labels.add("July");
+        labels.add("August");
+        labels.add("September");
+        labels.add("October");
+        labels.add("November");
+        labels.add("December");
+
+        /* for create Grouped Bar chart
+        ArrayList<BarEntry> group1 = new ArrayList<>();
+        group1.add(new BarEntry(4f, 0));
+        group1.add(new BarEntry(8f, 1));
+        group1.add(new BarEntry(6f, 2));
+        group1.add(new BarEntry(12f, 3));
+        group1.add(new BarEntry(18f, 4));
+        group1.add(new BarEntry(9f, 5));
+
+        ArrayList<BarEntry> group2 = new ArrayList<>();
+        group2.add(new BarEntry(6f, 0));
+        group2.add(new BarEntry(7f, 1));
+        group2.add(new BarEntry(8f, 2));
+        group2.add(new BarEntry(12f, 3));
+        group2.add(new BarEntry(15f, 4));
+        group2.add(new BarEntry(10f, 5));
+
+        BarDataSet barDataSet1 = new BarDataSet(group1, "Group 1");
+        //barDataSet1.setColor(Color.rgb(0, 155, 0));
+        barDataSet1.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarDataSet barDataSet2 = new BarDataSet(group2, "Group 2");
+        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        ArrayList<BarDataSet> dataset = new ArrayList<>();
+        dataset.add(barDataSet1);
+        dataset.add(barDataSet2);
+        */
+
+        BarData data = new BarData(labels, dataset);
+        int[] color = {
+                Color.rgb(241, 189, 157), Color.rgb(61, 146, 200), Color.rgb(53, 62, 112),
+                Color.rgb(122, 198, 174)
+        };
+        dataset.setColors(color);
+        barChart.setData(data);
+        barChart.animateY(5000);
+        barChart.setDescription("");
+
+
+    }
+}
---
+++ PieChart/PieChart/app/src/main/java/app/num/piechart/MainActivity.java
@@ -0,0 +1,67 @@
+package app.num.piechart;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+
+import java.util.ArrayList;
+import java.util.Random;
+public class MainActivity extends AppCompatActivity {
+    private Random random;//用於產生隨機數
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PieChart pieChart = (PieChart) findViewById(R.id.chart);
+        random = new Random();//隨機數
+        ArrayList<Entry> entries = new ArrayList<>(); //數值填入
+        for (int i = 0; i < 6; i++) {
+            float profit = random.nextFloat() * 100;
+//entries.add(BarEntry(float val,int positon);
+            entries.add(new Entry(profit, i));
+           // xVals.add((i + 1) + "月");
+        }
+
+
+        /*entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(12f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));*/
+        PieDataSet dataset = new PieDataSet(entries, "各類別分析");//類別填入
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("Breafast");
+        labels.add("Lunch");
+        labels.add("Dinner");
+        labels.add("traffic");
+        labels.add("snack");
+        labels.add("clothes");
+
+        PieData data = new PieData(labels, dataset);
+        int[] color = {
+                Color.rgb(241, 189, 157), Color.rgb(61, 146, 200), Color.rgb(53, 62, 112),
+                Color.rgb(122, 198, 174)
+        };
+
+
+        dataset.setColors(color);
+        dataset.setSliceSpace(5f);
+        dataset.setValueTextColor(Color.rgb(40,38,44));
+        dataset.setValueTextSize(18f);//
+
+        pieChart.setDescription("");
+        pieChart.setData(data);
+
+        pieChart.animateY(5000);
+        pieChart.saveToGallery("/sd/mychart.jpg", 85); // 85 is the quality of the image
+
+
+
+    }
+}
