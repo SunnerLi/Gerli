e377044f30f8bf61e044238d22c5e945c92a864d
SunnerLi <a6214123@gmail.com>
2016-11-22 23:03:11 +0800

    delete some useless code

--- server/syntaxJudge.py
+++ server/syntaxJudge.py
@@ -9,43 +9,11 @@ import numpy as np
 """
 
 # Constants
-assignKeyword = "is"
-activeKeyword = "spend"
-similarThreshold = 0.5
 embeddedSize = 100
 
 # Variables
 word_2_id = {"is" : 0, "spend" : 1}
 wordEmbedded = np.array([[1, 0], [0, 1]])
-
-"""
-def syntaxTypeJudge(subject, verb, _object, value):
-    
-        main function
-
-        Arg:    The subject, verb, object of the sentence,
-                The value of the transection
-        Ret:    The record object
-    
-    sp.show("Sub: ", subject)
-    sp.show("Vb: ", verb)
-    sp.show("Obj: ", _object)
-    sp.show("Val: ", value)
-    sim = np.zeros(3)
-
-    # Judge the type of the verb
-    sim[0] = similarity(verb, assignKeyword)
-    sim[1] = similarity(verb, activeKeyword)
-
-    # Maximum likelihood estimation
-    if np.max(sim) < similarThreshold:
-        return None
-    if sim[0] == np.max(sim):
-        record = Record(subject, value)
-    if sim[1] == np.max(sim):
-        record = Record(_object, value)
-    return record
-"""
 
 def similarity(string1, string2):
     """
