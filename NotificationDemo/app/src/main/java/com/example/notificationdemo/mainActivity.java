5f752d3333eda5d6aed6a52c68e676b1a35a7267
msn9110 <msn9110@gmail.com>
2016-12-04 22:02:11 +0800

    service at startup

---
+++ NotificationDemo/app/src/main/java/com/example/notificationdemo/mainActivity.java
@@ -0,0 +1,33 @@
+package com.example.notificationdemo;
+
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class mainActivity extends Activity{
+
+    final int notifyID = 1; // 通知的識別號碼
+    NotificationManager notificationManager;
+    Notification notification;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE); // 取得系統的通知服務
+        // 建立通知
+        notification = new Notification.Builder(getApplicationContext()).setSmallIcon(R.mipmap.ic_launcher).setContentTitle("內容標題").setContentText("內容文字").build();
+        Button btnNotice = (Button)findViewById(R.id.button);
+        btnNotice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notificationManager.notify(notifyID, notification); // 發送通知
+            }
+        });
+
+    }
+}
