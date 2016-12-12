2d39a66081c0afbbf54db70b9c281b5e91b1d7ef
msn9110 <msn9110@gmail.com>
2016-12-12 22:18:41 +0800

    提醒功能完成

--- NotificationDemo/app/src/main/java/com/example/notificationdemo/SetAlarmService.java
+++ NotificationDemo/app/src/main/java/com/example/notificationdemo/SetAlarmService.java
@@ -1,19 +1,35 @@
 package com.example.notificationdemo;
 
 import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
+
+import com.gerli.handsomeboy.*;
+import com.gerli.handsomeboy.gerlisqlitedemo.GerliDatabaseManager;
 
 
 public class SetAlarmService extends Service {
+    final static String TAG = "##SetAlarmService";
+    final static int MORNING = 11;
+    final static int AFTERNOON = 17;
+    final static int NIGHT = 22;
+    public final static String ACTION_BROADCAST = "ACTION_MY_BROADCAST";
+    final static ArrayList<Integer> alarmHour = new ArrayList<>(Arrays.asList(MORNING, AFTERNOON, NIGHT)) ;
+    final static int alarmMinute = 30;;
+    Thread doInBackground;
+    long today, morning, afternoon, night;
+
+    GerliDatabaseManager manager;
 
     @Nullable
     @Override
@@ -24,26 +40,103 @@ public class SetAlarmService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate() executed");
+        setTimes();
+        manager = new GerliDatabaseManager(this);
+        if(doInBackground == null){
+            doInBackground = new Thread(runnable);
+            doInBackground.start();
+        }
+    }
 
-        final int notifyID = 1; // 通知的識別號碼
-        NotificationManager notificationManager;
-        Notification notification;
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand() executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
 
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE); // 取得系統的通知服務
-        // 建立通知
-        notification = new Notification.Builder(getApplicationContext()).setSmallIcon(R.mipmap.ic_launcher).setContentTitle("NotificationDemo").setContentText("setAlarm").build();
-        notificationManager.notify(notifyID, notification); // 發送通知
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() executed");
+        isContinue = false;
+        try {
+            doInBackground.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-        Calendar cal = Calendar.getInstance();
-        // 設定於 30 sec 後執行
-        cal.add(Calendar.SECOND, 30);
+    boolean isContinue = true;
+    Runnable runnable = new Runnable() {
 
-        Intent intent = new Intent(this, PlayReceiver.class);
-        intent.putExtra("msg", "my_notification");
+        @Override
+        public void run() {
+            while (isContinue){
+                Calendar cal = Calendar.getInstance();
+                // 設定於 1 min 後執行
+                cal.add(Calendar.MINUTE, 1);
+                int alarmH = cal.get(Calendar.HOUR_OF_DAY);
+                int alarmMin = cal.get(Calendar.MINUTE);
+                int seconds = cal.get(Calendar.SECOND);
+                //Log.d(TAG, String.valueOf(alarmH) + ":" +String.valueOf(alarmMin));
+                if(alarmHour.contains(alarmH) && alarmMinute == alarmMin && seconds == 0 /**/&& !hasDone(cal.getTimeInMillis()) /**/){
+                    Log.d(TAG, "SetAlarm");
+                    Intent intent = new Intent(ACTION_BROADCAST);
 
-        PendingIntent pi = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_ONE_SHOT);
+                    PendingIntent pi = PendingIntent.getBroadcast(SetAlarmService.this, 1, intent, PendingIntent.FLAG_ONE_SHOT);
 
-        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pi);
+                    AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                    am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pi);
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private boolean hasDone(long currentTime){
+        boolean done = false;
+        int mode = 0;
+        long base = 0;
+        if(today <= currentTime && currentTime < morning){
+            mode = 1;
+            base = today;
+        } else if(morning <= currentTime && currentTime < afternoon){
+            mode = 2;
+            base = morning;
+        } else if(afternoon <= currentTime && currentTime < night){
+            mode = 3;
+            base = afternoon;
+        }
+
+        long latestTime = manager.getLatestRecordTime().getTime();
+        switch (mode){
+            case 1:
+            case 2:
+            case 3:
+                if (latestTime - base >= 0)
+                    done = true;
+                break;
+        }
+        return done;
+    }
+
+    private void setTimes(){
+        Date mtoday = new Date();
+        mtoday.setSeconds(0);
+        mtoday.setMinutes(0);
+        mtoday.setHours(0);
+        today = mtoday.getTime();
+        mtoday.setMinutes(alarmMinute);
+        mtoday.setHours(MORNING);
+        morning = mtoday.getTime();
+        mtoday.setHours(AFTERNOON);
+        afternoon = mtoday.getTime();
+        mtoday.setHours(NIGHT);
+        night = mtoday.getTime();
     }
 }
--- NotificationDemo/app/src/main/java/com/example/notificationdemo/mainActivity.java
+++ NotificationDemo/app/src/main/java/com/example/notificationdemo/mainActivity.java
@@ -1,34 +1,81 @@
 package com.example.notificationdemo;
 
 
-import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class mainActivity extends AppCompatActivity{
+public class mainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    final int notifyID = 1; // 通知的識別號碼
-    NotificationManager notificationManager;
-    Notification notification;
+    final static String TAG = "##mainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE); // 取得系統的通知服務
-        // 建立通知
-        notification = new Notification.Builder(getApplicationContext()).setSmallIcon(R.mipmap.ic_launcher).setContentTitle("內容標題").setContentText("內容文字").build();
-        Button btnNotice = (Button)findViewById(R.id.button);
-        btnNotice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                notificationManager.notify(notifyID, notification); // 發送通知
-            }
-        });
 
+        Button btnNotice = (Button)findViewById(R.id.button);
+        btnNotice.setOnClickListener(this);
+        setPreference();
+
+    }
+
+    private void setPreference(){
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean b = sharedPrefs.getBoolean("notice",false);
+        Intent intent = new Intent(mainActivity.this,SetAlarmService.class);
+        if(b){
+            startService(intent);
+        } else {
+            stopService(intent);
+        }
+    }
+
+    private static final int RESULT_SETTINGS = 1;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.menu_settings:
+                Intent i = new Intent(this, UserSettingActivity.class);
+                startActivityForResult(i, RESULT_SETTINGS);
+                break;
+
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                final int notifyID = 1;
+                final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE); // 取得系統的通知服務
+                // 建立通知
+                final Notification notification = new Notification.Builder(getApplicationContext())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("NotificationDemo")
+                        .setContentText("該記帳囉").build();
+                notificationManager.notify(notifyID, notification); // 發送通知
+                break;
+        }
     }
 }
