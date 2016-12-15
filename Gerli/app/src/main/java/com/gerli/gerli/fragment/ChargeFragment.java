d92be594e49034edda69859747199fc107d60ccb
SunnerLi <a6214123@gmail.com>
2016-12-15 22:36:12 +0800

    revise the button

--- Gerli/app/src/main/java/com/gerli/gerli/fragment/ChargeFragment.java
+++ Gerli/app/src/main/java/com/gerli/gerli/fragment/ChargeFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -33,7 +34,7 @@ public class ChargeFragment extends Fragment implements DatePickerFragment.PassO
     private GerliDatabaseManager gerliDatabaseManager;
     private View myView;
 
-    Button buttonInput, chatInput, voiceInput;
+    ImageButton buttonInput, chatInput, voiceInput;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,9 +85,9 @@ public class ChargeFragment extends Fragment implements DatePickerFragment.PassO
      */
 
     public void setButton(){
-        buttonInput = (Button) myView.findViewById(R.id.buttonInput);
-        voiceInput = (Button) myView.findViewById(R.id.voiceInput);
-        chatInput = (Button) myView.findViewById(R.id.chatInput);
+        buttonInput = (ImageButton) myView.findViewById(R.id.buttonInput);
+        voiceInput = (ImageButton) myView.findViewById(R.id.voiceInput);
+        chatInput = (ImageButton) myView.findViewById(R.id.chatInput);
 
         buttonInput.setOnClickListener(new View.OnClickListener() {
             @Override
