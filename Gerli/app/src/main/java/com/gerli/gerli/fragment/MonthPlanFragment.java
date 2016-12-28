f2a72563847f6456dda9a16b5767d9c169eb8260
s91503486f <s91503486f@yahoo.com.tw>
2016-12-29 01:06:55 +0800

    pull

--- Gerli/app/src/main/java/com/gerli/gerli/fragment/MonthPlanFragment.java
+++ Gerli/app/src/main/java/com/gerli/gerli/fragment/MonthPlanFragment.java
@@ -1,6 +1,7 @@
 package com.gerli.gerli.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.gerli.gerli.R;
+import com.gerli.gerli.btnInput.BtnInputActivity;
 
 
 /**
@@ -24,6 +26,13 @@ public class MonthPlanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Intent intent = new Intent(getActivity(), BtnInputActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putDouble("DOLLAR",0.0);
+        bundle.putBoolean("INOUT",true);
+        intent.putExtras(bundle);
+        startActivity(intent);
+
         return inflater.inflate(R.layout.fragment_month_plan, container, false);
     }
 
